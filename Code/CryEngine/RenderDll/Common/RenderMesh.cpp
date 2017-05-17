4baabc01145079dfb4e002e56c30a90221ab3490
uniflare <uniflare@gmail.com>
2017-05-17 01:12:02 +0100

    !BE (Renderer) (uniflare) nullptr safety. Prevents exit crash.

--- Code/CryEngine/RenderDll/Common/RenderMesh.cpp
+++ Code/CryEngine/RenderDll/Common/RenderMesh.cpp
@@ -4223,6 +4223,9 @@ void CRenderMesh::UpdateModified()
 // Mesh garbage collector
 void CRenderMesh::Tick(uint numFrames)
 {
+	assert(gRenDev->m_pRT != nullptr);
+	if (!gRenDev->m_pRT)
+		return;
 	MEMORY_SCOPE_CHECK_HEAP();
 	ASSERT_IS_RENDER_THREAD(gRenDev->m_pRT)
 		bool bKeepSystem = false;
